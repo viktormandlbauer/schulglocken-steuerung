6337c734a9ba832806784ff3f2acb10ca2e68efa
viktormandlbauer <viktor.mandlbauer@protonmail.com>
2022-10-15 13:45:49 +0200

    Network class implementation for header filer

--- include/Network.h
+++ include/Network.h
@@ -1,3 +1,29 @@
-void init_network();
-bool use_static_IP();
-bool use_DHCP();
+#include <Arduino.h>
+#include <EtherCard.h>
+
+class Network
+{
+private:
+    bool dhcp;
+    byte *ip_address;
+    byte *mac_address;
+    byte *gw_address;
+    byte *subnetmask;
+    byte *dns_address;
+
+public:
+    bool connected;
+
+    Network();
+    Network(byte ip_address[4], byte gw_address[4], byte dns_address[4], byte subnetmask[4]);
+
+    void set_ip(byte ip_address[4]);
+    byte *get_ip();
+    void set_gw(byte gw_address[4]);
+    byte *get_gw();
+    void set_dns(byte dns_address[4]);
+    byte *get_dns();
+    bool init_network();
+    bool test_gateway();
+    bool test_ntp();
+};
--- src/Network.cpp
+++ src/Network.cpp
@@ -1,114 +1,115 @@
 #define DEBUG
 
-#include <Arduino.h>
-#include <EtherCard.h>
 #include "Network.h"
 
 #define CHIP_SELECT 14
 
 byte Ethernet::buffer[700];
 
-class Network
+Network::Network()
 {
-private:
-    bool dhcp;
-    byte *ip_address;
-    byte *mac_address;
-    byte *gw_address;
-    byte *subnetmask;
-    byte *dns_address;
-    bool connected;
+    dhcp = true;
+    connected = false;
+}
 
-public:
-    Network()
+Network::Network(byte ip_address[4], byte gw_address[4], byte dns_address[4], byte subnetmask[4])
+{
+    ip_address = ip_address;
+    gw_address = gw_address;
+    dns_address = gw_address;
+    subnetmask = subnetmask;
+    connected = false;
+    dhcp = false;
+}
+
+void Network::set_ip(byte ip_address[4])
+{
+    this->ip_address = ip_address;
+}
+byte *Network::get_ip()
+{
+    return this->ip_address;
+}
+
+void Network::set_gw(byte gw_address[4])
+{
+    this->gw_address = gw_address;
+}
+
+byte *Network::get_gw()
+{
+    return this->ip_address;
+}
+
+void Network::set_dns(byte dns_address[4])
+{
+    this->dns_address = dns_address;
+}
+
+byte *Network::get_dns()
+{
+    return this->dns_address;
+}
+
+bool Network::init_network()
+{
+#ifdef DEBUG
+    Serial.println("[Info] Ethernet wird aktiviert ...");
+#endif
+    if (ether.begin(sizeof Ethernet::buffer, this->mac_address, CHIP_SELECT) == 0)
     {
-        this->dhcp = true;
         this->connected = false;
+        return false;
+#ifdef DEBUG
+        Serial.println("[Error] Ethernet konnte nicht aktiviert werden.");
+#endif
     }
+#ifdef DEBUG
+    Serial.println("[Info] Ethernet wurde aktiviert.");
+#endif
 
-    Network(byte ip_address[4], byte gw_address[4], byte dns_address[4])
-    {
-        this->ip_address = ip_address;
-        this->gw_address = gw_address;
-        this->dns_address = gw_address;
-        this->subnetmask = subnetmask;
-        this->connected = false;
-        this->dhcp = false;
-    }
-
-#pragma region setter getter methods
-    void set_ip(byte ip_address[4])
-    {
-        this->ip_address = ip_address;
-    }
-    byte *get_ip()
-    {
-        return this->ip_address;
-    }
-
-    void set_gw(byte gw_address[4])
-    {
-        this->gw_address = gw_address;
-    }
-
-    byte *get_gw()
-    {
-        return this->ip_address;
-    }
-
-    void set_dns(byte dns_address[4])
-    {
-        this->dns_address = dns_address;
-    }
-
-    byte *get_dns()
-    {
-        return this->dns_address;
-    }
-#pragma endregion
-
-    bool init_network()
+    if (dhcp)
     {
 #ifdef DEBUG
-        Serial.println("[Info] Ethernet wird aktiviert ...");
+        Serial.println(F("[Info] Setup DHCP"));
 #endif
-        if (ether.begin(sizeof Ethernet::buffer, this->mac_address, CHIP_SELECT) == 0)
+        if (!ether.dhcpSetup())
         {
-            this->connected = false;
+#ifdef DEBUG
+            Serial.println("[Error] DHCP ist fehlgeschlagen");
+#endif
             return false;
-#ifdef DEBUG
-            Serial.println("[Error] Ethernet konnte nicht aktiviert werden.");
-#endif
         }
-#ifdef DEBUG
-        Serial.println("[Info] Ethernet wurde aktiviert.");
-#endif
-
-        if (dhcp)
-        {
-#ifdef DEBUG
-            Serial.println(F("[Info] Setup DHCP"));
-#endif
-            if (!ether.dhcpSetup())
-            {
-#ifdef DEBUG
-                Serial.println("[Error] DHCP ist fehlgeschlagen");
-#endif
-                return -1;
-            }
-        }
-        else
-        {
-#ifdef DEBUG
-            Serial.println(F("[Info] Setup Static-IP"));
-#endif
-            ether.staticSetup(this->ip_address, this->gw_address, this->dns_address, this->subnetmask);
-        }
-#ifdef DEBUG
-        ether.printIp("[Info] IP-Adressse   : ", ether.myip);
-        ether.printIp("[Info] Netmask       : ", ether.netmask);
-        ether.printIp("[Info] Gateway       : ", ether.gwip);
-        ether.printIp("[Info] DNS           : ", ether.dnsip);
-#endif
     }
-};
+    else
+    {
+#ifdef DEBUG
+        Serial.println(F("[Info] Setup Static-IP"));
+#endif
+        ether.staticSetup(this->ip_address, this->gw_address, this->dns_address, this->subnetmask);
+    }
+#ifdef DEBUG
+    ether.printIp("[Info] IP-Adressse   : ", ether.myip);
+    ether.printIp("[Info] Netmask       : ", ether.netmask);
+    ether.printIp("[Info] Gateway       : ", ether.gwip);
+    ether.printIp("[Info] DNS           : ", ether.dnsip);
+#endif
+    return true;
+}
+
+bool Network::test_gateway()
+{
+    while (ether.clientWaitingGw())
+    {
+    }
+    ether.packetLoop(ether.packetReceive());
+    Serial.println("Gateway found");
+
+    return true;
+}
+
+bool Network::test_ntp()
+{
+
+    return true;
+}
